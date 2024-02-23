9b991b2a4f5fb8c49ac5e6c7df4280ee1d256d76
itout-datetoya <b1122502@edu.kit.ac.jp>
2024-02-23 17:59:06 +0900

    add code10

---
+++ code10/helper.py
@@ -0,0 +1,198 @@
+import unittest
+import hashlib
+
+
+SIGHASH_ALL = 1
+SIGHASH_NONE = 2
+SIGHASH_SINGLE = 3
+BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+TWO_WEEKS = 60 * 60 * 24 * 14
+MAX_TARGET = 0xffff * 256**(0x1d - 3)
+
+
+def run(test):
+    suite = unittest.TestSuite()
+    suite.addTest(test)
+    unittest.TextTestRunner().run(suite)
+
+
+# tag::source4[]
+def hash160(s):
+    '''sha256 followed by ripemd160'''
+    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()  # <1>
+# end::source4[]
+
+
+def hash256(s):
+    '''two rounds of sha256'''
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+
+# tag::source2[]
+def encode_base58(s):
+    count = 0
+    for c in s:  # <1>
+        if c == 0:
+            count += 1
+        else:
+            break
+    num = int.from_bytes(s, 'big')
+    prefix = '1' * count
+    result = ''
+    while num > 0:  # <2>
+        num, mod = divmod(num, 58)
+        result = BASE58_ALPHABET[mod] + result
+    return prefix + result  # <3>
+# end::source2[]
+
+
+# tag::source3[]
+def encode_base58_checksum(b):
+    return encode_base58(b + hash256(b)[:4])
+# end::source3[]
+
+
+def decode_base58(s):
+    num = 0
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+    combined = num.to_bytes(25, byteorder='big')
+    checksum = combined[-4:]
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError('bad address: {} {}'.format(checksum, hash256(combined[:-4])[:4]))
+    return combined[1:-4]
+
+
+def little_endian_to_int(b):
+    '''little_endian_to_int takes byte sequence as a little-endian number.
+    Returns an integer'''
+    return int.from_bytes(b, 'little')
+
+
+def int_to_little_endian(n, length):
+    '''endian_to_little_endian takes an integer and returns the little-endian
+    byte sequence of length'''
+    return n.to_bytes(length, 'little')
+
+def read_varint(s):
+    '''read_varint reads a variable integer from a stream'''
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0xfd means the next two bytes are the number
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        # 0xfe means the next four bytes are the number
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        # 0xff means the next eight bytes are the number
+        return little_endian_to_int(s.read(8))
+    else:
+        # anything else is just the integer
+        return i
+
+
+def encode_varint(i):
+    '''encodes an integer as a varint'''
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
+    
+def h160_to_p2pkh_address(h160, testnet=False):
+        if testnet:
+            prefix = b'\x6f'
+        else:
+            prefix = b'\x00'
+        return encode_base58_checksum(prefix + h160)
+
+def h160_to_p2sh_address(h160, testnet=False):
+        if testnet:
+            prefix = b'\xc4'
+        else:
+            prefix = b'\x05'
+        return encode_base58_checksum(prefix + h160)
+
+def bits_to_target(bits):
+    exponent = bits[-1]
+    coefficient = little_endian_to_int(bits[:-1])
+    return coefficient * 256**(exponent-3)
+
+def target_to_bits(target):
+    raw_bytes = target.to_bytes(32, 'big')
+    raw_bytes = raw_bytes.lstrip(b'\x00')
+    if raw_bytes[0] > 0x7f:
+        exponent = len(raw_bytes) + 1
+        coefficient = b'\x00' + raw_bytes[:2]
+    else:
+        exponent = len(raw_bytes)
+        coefficient = raw_bytes[:3]
+    new_bits = coefficient[::-1] + bytes([exponent])
+    return new_bits
+
+def calculate_new_bits(prev_bits, time_differential):
+    if time_differential > TWO_WEEKS * 4:
+        time_differential = TWO_WEEKS * 4
+    if time_differential < TWO_WEEKS // 4:
+        time_differential = TWO_WEEKS // 4
+    new_target = bits_to_target(prev_bits) * time_differential // TWO_WEEKS
+    if new_target > MAX_TARGET:
+        new_target = MAX_TARGET
+    return target_to_bits(new_target)
+
+
+
+class HelperTest(unittest.TestCase):
+
+    def test_little_endian_to_int(self):
+        h = bytes.fromhex('99c3980000000000')
+        want = 10011545
+        self.assertEqual(little_endian_to_int(h), want)
+        h = bytes.fromhex('a135ef0100000000')
+        want = 32454049
+        self.assertEqual(little_endian_to_int(h), want)
+
+    def test_int_to_little_endian(self):
+        n = 1
+        want = b'\x01\x00\x00\x00'
+        self.assertEqual(int_to_little_endian(n, 4), want)
+        n = 10011545
+        want = b'\x99\xc3\x98\x00\x00\x00\x00\x00'
+        self.assertEqual(int_to_little_endian(n, 8), want)
+
+    def test_base58(self):
+        addr = 'mnrVtF8DWjMu839VW3rBfgYaAfKk8983Xf'
+        h160 = decode_base58(addr).hex()
+        want = '507b27411ccf7f16f10297de6cef3f291623eddf'
+        self.assertEqual(h160, want)
+        got = encode_base58_checksum(b'\x6f' + bytes.fromhex(h160))
+        self.assertEqual(got, addr)
+
+    def test_p2pkh_address(self):
+        h160 = bytes.fromhex('74d691da1574e6b3c192ecfb52cc8984ee7b6c56')
+        want = '1BenRpVUFK65JFWcQSuHnJKzc4M8ZP8Eqa'
+        self.assertEqual(h160_to_p2pkh_address(h160, testnet=False), want)
+        want = 'mrAjisaT4LXL5MzE81sfcDYKU3wqWSvf9q'
+        self.assertEqual(h160_to_p2pkh_address(h160, testnet=True), want)
+
+    def test_p2sh_address(self):
+        h160 = bytes.fromhex('74d691da1574e6b3c192ecfb52cc8984ee7b6c56')
+        want = '3CLoMMyuoDQTPRD3XYZtCvgvkadrAdvdXh'
+        self.assertEqual(h160_to_p2sh_address(h160, testnet=False), want)
+        want = '2N3u1R6uwQfuobCqbCgBkpsgBxvr1tZpe7B'
+        self.assertEqual(h160_to_p2sh_address(h160, testnet=True), want)
+    
+    def test_calculate_new_bits(self):
+        prev_bits = bytes.fromhex('54d80118')
+        time_differential = 302400
+        want = bytes.fromhex('00157617')
+        self.assertEqual(calculate_new_bits(prev_bits, time_differential), want)
+
+if __name__ == "__main__":
+    unittest.main()
